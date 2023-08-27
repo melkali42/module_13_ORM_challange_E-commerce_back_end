a150fd6e17f1437db555be5df52733c70d750583
Melissa <melissakalish2@gmail.com>
2023-08-27 13:37:52 -0500

    added code to server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const routes = require('./routes');
+
+//Import the sequelize connection
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+//sync the sequelize models to the database, then turn on the server
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => {
+        console.log(`App listening on port ${PORT}!`);
+    });
+})
